0d4096e8063a273994520e8929cdde2f994470a7
vaibhav <vaibhavk1965@gmail.com>
2024-07-18 18:16:36 +0530

    ..

--- src/lib/pg.ts
+++ src/lib/pg.ts
@@ -1,20 +1,15 @@
 import {Client} from "pg";
 
-const {DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PWD, DB_URL} = process.env;
+const {DB_URL} = process.env;
 
 const pg = new Client({
- host: DB_HOST,
- port: Number(DB_PORT),
- user: DB_USER,
- database: DB_NAME,
- password: DB_PWD,
  connectionString: DB_URL,
  ssl: {
   rejectUnauthorized: false
  }
 });
 
-pg.connect(() => console.log("CONNECTED_PG"));
+pg.connect();
 
 pg.on("error",(err) => console.log(err));
 
