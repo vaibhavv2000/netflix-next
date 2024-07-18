197f25d8364bb856e4dbc094d08149e35d13c7fe
vaibhav <vaibhavk1965@gmail.com>
2024-07-18 20:01:52 +0530

    updated url

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://netflix-next-pearl.vercel.app";
+
 export const API = axios.create({
-  baseURL: "https://netflix-next-pearl.vercel.app/api",
+  baseURL: `${URL}/api`,
   withCredentials: true,
 });
